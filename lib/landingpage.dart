7233ea027006e77ffbef3476c394384c2506ed7c
CYJ <saeyoon1887@gmail.com>
2023-05-19 21:07:15 +0900

    랜딩페이지 자동 넘기기

--- lib/landingpage.dart
+++ lib/landingpage.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:kiting/mainpage.dart';
 
 class LandingPage extends StatefulWidget {
 
@@ -8,12 +12,27 @@ class LandingPage extends StatefulWidget {
 
 class _LandingPageState extends State<LandingPage> {
   @override
+  void initState() {
+    Timer(Duration(seconds: 3),(){
+      Get.offAll(MainPage());
+    });
+    // TODO: implement initState
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Image.asset('assets/image/landing.png')
+      body: Stack(
+        alignment: Alignment.center,
+        children: [Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset('assets/image/landing.png',fit:  BoxFit.cover,)
 
-      ),
+          ),
+          CircularProgressIndicator()
+        ],
+      )
     );
   }
 }
