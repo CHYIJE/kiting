eb6b3638f9ce8a3ad05a7c369f3b2162d6a6bdda
CHYIJE <wjdtpdbs1887@naver.com>
2023-05-31 16:28:14 +0900

    05.31 chat

--- lib/mainpages/homescreen.dart
+++ lib/mainpages/homescreen.dart
@@ -1,15 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:kiting/mainpages/showgridscreen.dart';
+import 'package:kiting/mainpages/mylikescreen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen>  {
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child:  Text("매칭화면."),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // 첫 번째 사진
+              Image.asset(
+                'assets/image/man.jpg',
+                width: 400 , // 원하는 너비로 조정
+                height: 500, // 원하는 높이로 조정
+              ),
+              // 두 번째 사진
+              Image.asset(
+                'assets/image/girl.jpg',
+                width: 400, // 원하는 너비로 조정
+                height: 500, // 원하는 높이로 조정
+              ),
+            ],
+          ),
+          SizedBox(height: 50), // 사진과 버튼 사이의 간격 조절을 위한 SizedBox 추가
+          // chat 버튼
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ShowGridScreen()),
+              );
+            },
+            child: Text('Chat'),
+          ),
+          SizedBox(height: 20), // 버튼과 버튼 사이의 간격 조절을 위한 SizedBox 추가
+          // like 버튼
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MylikeScreen()),
+              );
+            },
+            child: Text('Like'),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/mainpages/showgridscreen.dart
+++ lib/mainpages/showgridscreen.dart
@@ -5,11 +5,87 @@ class ShowGridScreen extends StatefulWidget {
   _ShowGridScreenState createState() => _ShowGridScreenState();
 }
 
-class _ShowGridScreenState extends State<ShowGridScreen>  {
+class _ShowGridScreenState extends State<ShowGridScreen> {
+  List<String> messages = [];
+  TextEditingController _textEditingController = TextEditingController();
+  ScrollController _scrollController = ScrollController();
+
+  void _sendMessage() {
+    String message = _textEditingController.text;
+    setState(() {
+      messages.insert(0, message); // 가장 아래에 새로운 메시지 추가
+      _textEditingController.clear();
+    });
+
+    // 새로운 메시지가 추가될 때 스크롤을 가장 아래로 이동
+    _scrollController.animateTo(
+      0.0,
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child:  Text("채팅."),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('채팅'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              reverse: true,
+              controller: _scrollController,
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+                String message = messages[index];
+
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Text(
+                        message,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    CircleAvatar(
+                      backgroundImage: AssetImage('assets/image/man.jpg'),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _textEditingController,
+                    decoration: InputDecoration(
+                      hintText: '메시지 입력',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: _sendMessage,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
